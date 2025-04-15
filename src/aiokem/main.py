41eccfc27adac91e63ee2a88a14d557f5ae0554f
PeteRager <pete.sage@icloud.com>
2025-04-14 22:23:32 -0400

    update exception handling

--- src/aiokem/main.py
+++ src/aiokem/main.py
@@ -1,19 +1,29 @@
 """AioKem class for interacting with Kohler Energy Management System (KEM) API."""
 
+from http import HTTPStatus
 from typing import Any
 
-from aiohttp import ClientSession, ClientTimeout
+from aiohttp import ClientConnectionError, ClientSession, ClientTimeout
 from multidict import CIMultiDict
+from yarl import URL
 
-AUTHENTICATION_URL = "https://kohler-homeenergy.okta.com/oauth2/default/v1/token"
+from .exceptions import (
+    AuthenticationCredentialsError,
+    AuthenticationError,
+    CommunicationError,
+)
+
+AUTHENTICATION_URL = URL("https://kohler-homeenergy.okta.com/oauth2/default/v1/token")
 CLIENT_KEY = (
     "MG9hMXFpY3BkYWdLaXdFekYxZDg6d3Raa1FwNlY1T09vMW9"
     "PcjhlSFJHTnFBWEY3azZJaXhtWGhINHZjcnU2TWwxSnRLUE5obXdsMEN1MGlnQkVIRg=="
 )
 API_KEY = "pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp"
+
 API_BASE = (
     "https://az-amer-prod-hems-capp.jollyglacier-72224ec0.eastus.azurecontainerapps.io"
 )
+HOMES_URL = URL(f"{API_BASE}/kem/api/v3/homeowner/homes")
 
 
 class AioKem:
@@ -37,59 +47,93 @@ class AioKem:
             }
         )
 
-        data = {
-            "grant_type": "password",
-            "username": username,
-            "password": password,  # Replace 'pass' with the actual password
-            "scope": "openid profile offline_access email",
-        }
-
-        response = await self._session.post(
-            AUTHENTICATION_URL, headers=headers, data=data
+        data = CIMultiDict(
+            {
+                "grant_type": "password",
+                "username": username,
+                "password": password,
+                "scope": "openid profile offline_access email",
+            }
         )
-        response_data = await response.json()
-        if response.status != 200:
-            raise Exception(
-                f"Login failed: {response_data.get('error_description', 'unknown')}"
+
+        try:
+            response = await self._session.post(
+                AUTHENTICATION_URL, headers=headers, data=data
             )
-        self._token = response_data.get("access_token")
-        self._refresh_token = response_data.get("refresh_token")
+            response_data = await response.json()
+            if response.status != HTTPStatus.OK:
+                # Authentication returns 400 if the credentials are invalid
+                if response.status == HTTPStatus.BAD_REQUEST:
+                    raise AuthenticationCredentialsError(
+                        f"Invalid Credentials: "
+                        f"{response_data.get('error_description', 'unknown')} "
+                        f"Code {response.status}"
+                    )
+                else:
+                    raise AuthenticationError(
+                        f"Authentication failed: "
+                        f"{response_data.get('error_description', 'unknown')} "
+                        f"Code {response.status}"
+                    )
+            self._token = response_data.get("access_token")
+            if not self._token:
+                raise Exception("Login failed: No access token received")
+
+            self._refresh_token = response_data.get("refresh_token")
+            if not self._refresh_token:
+                raise Exception("Login failed: No refresh token received")
+
+        except ClientConnectionError as e:
+            raise AuthenticationError(f"Connection error: {e}") from e
+
+    async def _get_helper(self, url: URL) -> dict[str, Any] | list[dict[str, Any]]:
+        """Helper function to get data from the API."""
         if not self._token:
-            raise Exception("Login failed: No access token received")
-        if not self._refresh_token:
-            raise Exception("Login failed: No refresh token received")
+            raise CommunicationError("Not authenticated")
+        headers = CIMultiDict(
+            {
+                "apikey": API_KEY,
+                "authorization": f"bearer {self._token}",
+            }
+        )
+
+        try:
+            response = await self._session.get(url, headers=headers)
+            response_data = await response.json()
+            if response.status != 200:
+                if response.status == HTTPStatus.UNAUTHORIZED:
+                    raise AuthenticationError("Unauthorized: {response_data}")
+                else:
+                    raise Exception(
+                        f"Failed to fetch data: {response.status} {response_data}"
+                    )
+
+        except ClientConnectionError as e:
+            raise CommunicationError(f"Connection error: {e}") from e
+
+        if not response_data:
+            raise Exception("No data received")
+        return response_data
 
     async def get_homes(self) -> list[dict[str, Any]]:
         """Get the list of homes."""
-        url = f"{API_BASE}/kem/api/v3/homeowner/homes"
-        headers = {
-            "apikey": API_KEY,
-            "authorization": f"bearer {self._token}",
-        }
-
-        response = await self._session.get(url, headers=headers)
-        if response.status != 200:
-            raise Exception(f"Failed to fetch homes: {response.status}")
-        response_data = await response.json()
-        if not response_data:
-            raise Exception("No data received")
-        return response_data
+        response = await self._get_helper(HOMES_URL)
+        if not isinstance(response, list):
+            raise TypeError(
+                f"Expected a list of homes, but got a different type {type(response)}"
+            )
+        return response
 
     async def get_generator_data(self, generator_id: int) -> dict[str, Any]:
         """Get generator data for a specific generator."""
-        url = f"{API_BASE}/kem/api/v3/devices/{generator_id}"
-        headers = {
-            "apikey": API_KEY,
-            "authorization": f"bearer {self._token}",
-        }
-
-        response = await self._session.get(url, headers=headers)
-        if response.status != 200:
-            raise Exception(f"Failed to fetch generator data: {response.status}")
-        response_data = await response.json()
-        if not response_data:
-            raise Exception("No data received")
-        return response_data
+        url = URL(f"{API_BASE}/kem/api/v3/devices/{generator_id}")
+        response = await self._get_helper(url)
+        if not isinstance(response, dict):
+            raise TypeError(
+                "Expected a dictionary for generator data, "
+                f"but got a different type {type(response)}"
+            )
+        return response
 
     async def close(self) -> None:
         """Close the session."""
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,14 @@
+from http import HTTPStatus
 from unittest.mock import AsyncMock, Mock
 
 import pytest
+from aiohttp import ClientConnectionError
 
+from aiokem.exceptions import (
+    AuthenticationCredentialsError,
+    AuthenticationError,
+    CommunicationError,
+)
 from aiokem.main import AUTHENTICATION_URL, AioKem
 
 
@@ -36,3 +43,50 @@ async def test_login():
         "password": "password",
         "scope": "openid profile offline_access email",
     }
+
+
+@pytest.mark.asyncio
+async def test_login_exceptions():
+    # Create a mock session
+    mock_session = Mock()
+    mock_session.post = AsyncMock()
+    kem = AioKem(session=mock_session)
+
+    # Mock the response for the login method
+    mock_response = AsyncMock()
+    mock_response.status = HTTPStatus.BAD_REQUEST
+    mock_response.json.return_value = {
+        "error_description": "The credentials provided were invalid.",
+    }
+    mock_session.post.return_value = mock_response
+
+    # Call the login method
+    with pytest.raises(AuthenticationCredentialsError) as excinfo:
+        await kem.login("username", "password")
+
+    # Assert that the access token and refresh token are set correctly
+    assert kem._token is None
+    assert kem._refresh_token is None
+    # Assert that the exception message is correct
+    assert (
+        str(excinfo.value)
+        == "Invalid Credentials: The credentials provided were invalid. Code 400"
+    )
+
+    mock_response = AsyncMock()
+    mock_response.status = HTTPStatus.FORBIDDEN
+    mock_response.json.return_value = {
+        "error_description": "Disallowed operation.",
+    }
+    mock_session.post.return_value = mock_response
+    # Call the login method
+    with pytest.raises(AuthenticationError) as excinfo:
+        await kem.login("username", "password")
+    assert str(excinfo.value) == "Authentication failed: Disallowed operation. Code 403"
+
+    mock_session.post.side_effect = ClientConnectionError("Internet connection error")
+
+    # Call the login method
+    with pytest.raises(CommunicationError) as excinfo:
+        await kem.login("username", "password")
+    assert str(excinfo.value) == "Connection error: Internet connection error"
